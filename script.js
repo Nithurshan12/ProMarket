9b40e43f893787a0edc286181091f36dc896526e
Nithurshan12 <Nithurshan.sritharan@student.tdsb.on.ca>
2025-05-10 22:18:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+
+function filterByCategory() {
+    const selectedCategory = document.getElementById('category').value;
+    const products = document.querySelectorAll('.product');
+
+    products.forEach(product => {
+        const productCategory = product.getAttribute('data-category');
+        if (selectedCategory === 'all' || productCategory === selectedCategory) {
+            product.style.display = 'block';
+        } else {
+            product.style.display = 'none';
+        }
+    });
+}
 // Initialize an empty cart
 let cart = [];
 
